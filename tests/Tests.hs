f968a47110eebc8eb9fefc8ae50d69e9ac2b1cd6
Valentin Reis <vreis@anl.gov>
2019-04-19 16:56:15 -0500

    minor refactor

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -21,43 +21,49 @@ import           Protolude
 
 import           Dhrun.Types
 import           Dhrun.AesonTypes
-import           Data.ByteString.Char8
+import qualified Data.ByteString.Char8         as B8
 import           Control.Monad.Writer
 import           System.IO.Error
 
-data Result = Success | Failure
+data Result = Success | Failure deriving (Eq)
 testDY :: (MonadIO m, MonadWriter [Text] m) => Text -> m Result
 testDY fn = do
 
   tell ["loading " <> fn <> ".yml:"]
   loadedY <- decodeCfg $ "./examples/" <> fn <> "/" <> fn <> ".yml"
-  for_ (lines $ encodeCfg loadedY) $ \x -> tell [toS x]
+  for_ (B8.lines $ encodeCfg loadedY) $ \x -> tell [toS x]
 
   tell ["loading " <> fn <> ".dh:"]
   loadedD <- inputCfg $ "./examples/" <> fn <> "/" <> fn <> ".dh"
-  for_ (lines $ encodeCfg loadedD) $ \x -> tell [toS x]
+  for_ (B8.lines $ encodeCfg loadedD) $ \x -> tell [toS x]
 
   if loadedY == loadedD
     then tell ["success."] >> return Success
     else return Failure
 
 main :: IO ()
-main = for_ ["simple", "full", "two"] $ \testName ->
-  runWriterT (runExceptT (testDY testName)) >>= \case
-    (Right Failure, es) ->
-      putText
-          (  "Test \""
-          <> testName
-          <> "\" failure: .yml/.dh produced different configs.\nError log:"
-          )
-        >> for_ es putText
-    (Right Success, _) ->
-      putText
-        $  "Test \""
-        <> testName
-        <> "\" success: .yml/.dh produced identical configs."
-    (Left (e :: IOError), es) ->
-      putText ("Failure in test " <> testName <> " :")
-        >> print e
-        >> putText "with test log:"
-        >> for_ es putText
+main = do
+  fileLoadingResults <- for ["simple", "full", "two"] $ \testName ->
+    (runWriterT . runExceptT . testDY) testName >>= \case
+      (Right Failure, es) ->
+        putText
+            (  "Test \""
+            <> testName
+            <> "\" failure: .yml/.dh produced different configs.\nError log:"
+            )
+          >> for_ es putText
+          >> return Success
+      (Right Success, _) ->
+        putText
+            (  "Test \""
+            <> testName
+            <> "\" success: .yml/.dh produced identical configs."
+            )
+          >> return Success
+      (Left (e :: IOError), es) ->
+        putText ("Failure in test " <> testName <> " :")
+          >> print e
+          >> putText "with test log:"
+          >> for_ es putText
+          >> return Failure
+  when (Failure `elem` fileLoadingResults) $ die "test set failure."
