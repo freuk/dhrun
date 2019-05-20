bd979036545e347c0334c69b1e5edb85338d0f8b
Valentin Reis <vreis@anl.gov>
2019-05-20 13:47:31 -0500

    Fixing the exception throwing delay.

--- src/Dhrun/Conduit.hs
+++ src/Dhrun/Conduit.hs
@@ -1,4 +1,6 @@
-{-|
+{-# language DataKinds #-}
+{-# language FlexibleContexts #-}
+{-| 
 Module      : Dhrun.Run
 Description : runner
 Copyright   : (c) Valentin Reis, 2018
@@ -30,14 +32,14 @@ import           Data.Conduit
                    , yield
                    , await
                    , runConduit
-                   , fuseUpstream
                    , (.|)
                    )
 import qualified Data.Conduit.Process.Typed                        as PT
                    ( Process
                    , ProcessConfig
                    )
-
+import qualified Data.Conduit.Lift                                 as CLF
+import           Data.Time.Clock.POSIX
 
 type Source = ConduitT () ByteString IO ()
 type Sink = ConduitT ByteString Void IO ()
@@ -50,41 +52,77 @@ monitor
   -> ConduitT () ByteString IO ()
   -> ConduitT ByteString Void IO ()
   -> IO ()
-monitor behavior source sink = catch go $ \case
-  e@ThrowFoundAllWants    -> throw e
-  e@(ThrowFoundAnAvoid _) -> threadDelay 1000000 >> throw e
- where
-  go =
-    runConduit
-      $              source
-      .|             CB.lines
-      .|             makeBehavior behavior
-      `fuseUpstream` CC.unlinesAscii
-      `fuseUpstream` sink
+monitor behavior source sink =
+  runConduit
+    $  source
+    .| CB.lines
+    .| void (CLF.runStateC initialState (CLF.runReaderC behavior makeBehavior))
+    .| CC.unlinesAscii
+    .| sink
+
+initialState :: MonitoringState
+initialState = Nothing
+type MonitoringState = Maybe Matched
+data Matched = Matched
+  { matched :: Text
+  , atTime :: POSIXTime } deriving (Show)
 
 -- | makeBehavior builds an IO conduit that throws a PatternMatched when
 -- all wanted pattern or one avoided pattern are found
-makeBehavior :: (Monad m) => Check -> ConduitT ByteString ByteString m ()
-makeBehavior c = case wants c of
-  [] -> cleanLooper c
-  as -> expectfulLooper c $ toS <$> as
+makeBehavior
+  :: (MonadIO m, MonadState MonitoringState m, MonadReader Check m)
+  => ConduitT ByteString ByteString m ()
+makeBehavior = wants <$> ask >>= \case
+  [] -> cleanLooper
+  as -> expectfulLooper $ toS <$> as
 
 expectfulLooper
-  :: (Monad m) => Check -> [ByteString] -> ConduitT ByteString ByteString m ()
-expectfulLooper _ [] = throw ThrowFoundAllWants
-expectfulLooper c l  = noAvoidsAndOtherwise c $ \b ->
-  yield b >> expectfulLooper c (filter (not . flip B.isInfixOf b) (toS <$> l))
+  :: (MonadIO m, MonadState MonitoringState m, MonadReader Check m)
+  => [ByteString]
+  -> ConduitT ByteString ByteString m ()
+expectfulLooper [] = throw ThrowFoundAllWants
+expectfulLooper l  = noAvoidsAndOtherwise $ \b ->
+  yield b >> expectfulLooper (filter (not . flip B.isInfixOf b) (toS <$> l))
 
-cleanLooper :: (Monad m) => Check -> ConduitT ByteString ByteString m ()
-cleanLooper c = noAvoidsAndOtherwise c $ \b -> yield b >> cleanLooper c
+cleanLooper
+  :: (MonadIO m, MonadState MonitoringState m, MonadReader Check m)
+  => ConduitT ByteString ByteString m ()
+cleanLooper = noAvoidsAndOtherwise $ \b -> yield b >> cleanLooper
 
 noAvoidsAndOtherwise
-  :: (Monad m)
-  => Check
-  -> (ByteString -> ConduitT ByteString ByteString m ())
+  :: (MonadIO m, MonadState MonitoringState m, MonadReader Check m)
+  => (ByteString -> ConduitT ByteString ByteString m ())
   -> ConduitT ByteString ByteString m ()
-noAvoidsAndOtherwise Check {..} otherwiseConduit = await >>= \case
-  Just b -> case filter (`B.isInfixOf` b) (toS <$> avoids) of
-    []     -> otherwiseConduit b
-    xh : _ -> throw (ThrowFoundAnAvoid $ toS xh) >> otherwiseConduit b
-  Nothing -> return ()
+noAvoidsAndOtherwise otherwiseConduit = do
+  av <- avoids <$> ask
+  t  <- liftIO getPOSIXTime
+  get >>= \case
+    Just m  -> goThrow t m
+    Nothing -> return ()
+  await >>= \case
+    Just b -> case filter (`B.isInfixOf` b) (toS <$> av) of
+      []     -> otherwiseConduit b
+      xh : _ -> do
+        put . Just $ Matched (toS xh) t
+        otherwiseConduit b
+    Nothing -> return ()
+ where
+  goThrow :: POSIXTime -> Matched -> ConduitT ByteString ByteString m ()
+  goThrow t Matched {..} =
+    when (t > 1 + atTime) (throw $ ThrowFoundAnAvoid matched)
+
+{-noAvoidsAndOtherwise-}
+  {-:: (MonadIO m)-}
+  {-=> Maybe (Integer, a)-}
+  {--> Check-}
+  {--> (Maybe (Integer, a) -> ByteString -> ConduitT ByteString ByteString m ())-}
+  {--> ConduitT ByteString ByteString m ()-}
+{-noAvoidsAndOtherwise mt Check {..} otherwiseConduit = do-}
+  {-t <- liftIO askPOSIXTime <&> toInteger-}
+  {-case shouldThrow t mt of-}
+    {-Just xh -> throw $ ThrowFoundAnAvoid $ toS xh-}
+    {-Nothing -> await >>= \case-}
+      {-Just b -> case filter (`B.isInfixOf` b) (toS <$> avoids) of-}
+        {-[]     -> otherwiseConduit t b-}
+        {-xh : _ -> otherwiseConduit t b-}
+      {-Nothing -> return ()-}
